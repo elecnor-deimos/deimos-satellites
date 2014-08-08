1cb58b9989ee65c4285775e268c6c89eea2f1633
MatteoDiPaolo <matteo.del.passo89@gmail.com>
2014-08-08 11:26:15 +0200

    Info box

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
 	timeline: false,
 });
 
-var geojsonDataSource = new Cesium.GeoJsonDataSource();		
-viewer.dataSources.add(geojsonDataSource);
+viewer.scene.debugShowFramesPerSecond = true;
+
+var geojsonDataSource = new Cesium.GeoJsonDataSource();	
 
 
 
@@ -23,62 +24,139 @@ function success(){
 }
 
 
-//-------------------------------------------DATA SLIDER CREATION--------------------------------------------------------------
-function createDateSlider(startDate, stopDate){
-	
-	$("#dateSlider").dateRangeSlider({
-		bounds: {min: startDate, max: stopDate},
-		defaultValues: {min: startDate, max: new Date(new Date(startDate).setMonth(startDate.getMonth()+12))},
-	});	
-	importGeoJSON(startDate, new Date(new Date(startDate).setMonth(startDate.getMonth()+12)));
 
-	$("#dateSlider").bind("valuesChanged", function(e, data){			
-		
+//-------------------------------------------DATA SLIDER CREATION--------------------------------------------------------------
+function createDateSlider(startDate, stopDate){		
+	
+	$('#dateSlider').dateRangeSlider({
+		bounds: {
+			min: startDate, 
+			max: stopDate
+		},
+		defaultValues: {
+			min: startDate, 
+			max: new Date(new Date(startDate).setMonth(startDate.getMonth()+6))
+		},
+		range:{
+		    min: {days: 1},
+		    max: {years: 1}
+		 },
+	});	
+
+	$('#dateSlider').bind("valuesChanged", function(e, data){			
 		$('#loading').css({
 			display: 'block'
-		});
-		
+		});		
 		importGeoJSON(data.values.min, data.values.max);
 	});
 	
+	
+	$('#dateSlider >').css({
+		background: 'rgba(38, 38, 38, 0.75)',
+	});	
+	$('.ui-rangeSlider-bar').css({
+		background: 'rgba(100, 100, 0, 0.75)',
+	});		
+	
+	importGeoJSON(startDate, new Date(new Date(startDate).setMonth(startDate.getMonth()+6)));
+
 }
+
 
 
 //---------------------------------------------IMPORT GEOJSON------------------------------------------------
 function importGeoJSON(startDate, stopDate){	
 	
+	viewer.dataSources.remove(geojsonDataSource);
+	
 	var startDay = startDate.getDate();
 	var startMonth = startDate.getMonth();
 	var startYear = startDate.getFullYear();
-
+	
 	var stopDay = stopDate.getDate();
 	var stopMonth = stopDate.getMonth();
 	var stopYear = stopDate.getFullYear();
 		
 	if(startDay < 10)	startDay = '0'+startDay;
-	if(startMonth < 10)	startMonth = '0'+startMonth;
+	if(startMonth < 9)	startMonth = '0'+(startMonth+1);
+	else	startMonth +=1;
 	if(stopDay < 10)	stopDay = '0'+stopDay;
-	if(stopMonth < 10)	stopMonth = '0'+stopMonth;
+	if(stopMonth < 9)	stopMonth = '0'+(stopMonth+1);
+	else	stopMonth +=1;
 	
 	
-	var index = viewer.dataSources.indexOf(geojsonDataSource);
-	var promise = viewer.dataSources.get(index).loadUrl('http://dms-coverage.cartodb.com/api/v2/sql?'+
-										'format=GeoJSON'+
-										'&q=SELECT%20*%20FROM%20deimos_1_scenes%20where%20image_date%20BETWEEN%20'+
-										'%27'+ startYear + startMonth + startDay +'%2000:00:00.000%27'+
-										'%20AND%20'+
-										'%27'+ stopYear + stopMonth + stopDay +'%2000:00:00.000%27'+
-										'&api_key=cfe0a8d8ed214abdd0e1f735e77ac2972a76a916');
-	
-	promise.then(function(){
-		$('#loading').css({
-			display: 'none'
-		});
-	});	
-	promise.otherwise(function(){
-		$('#loading').css({
-			display: 'none'
-		});
-	});	
+	var promise = geojsonDataSource.loadUrl('http://dms-coverage.cartodb.com/api/v2/sql?'+
+											'format=GeoJSON'+
+											'&q=SELECT%20*%20FROM%20deimos_1_scenes%20where%20image_date%20BETWEEN%20'+
+											'%27'+ startYear + startMonth + startDay +'%2000:00:00.000%27'+
+											'%20AND%20'+
+											'%27'+ stopYear + stopMonth + stopDay +'%2000:00:00.000%27'+
+											'&api_key=cfe0a8d8ed214abdd0e1f735e77ac2972a76a916');
 			
+			
+	promise.then(function(){	
+		$('#loading').css({
+			display: 'none'
+		});		
+		infoBox(true);
+		viewer.dataSources.add(geojsonDataSource);
+		changeOutlineColor();
+	});	
+	promise.otherwise(function(){	
+		console.log('otherwise');
+		$('#loading').css({
+			display: 'none'
+		});		
+		infoBox(false);
+		viewer.dataSources.remove(geojsonDataSource);
+	});	
+	
+	
 }
+
+
+
+//--------------------------------------------CHANGE OUTLINE COLOR--------------------------------------
+function changeOutlineColor(){
+
+	var outlineColor = new Cesium.Color(0.4, 0.5, 0.5, 0.3);
+    for (var i = 0; i < geojsonDataSource.entities.entities.length; i++) {   
+    	geojsonDataSource.entities.entities[i].polygon.outlineColor = new Cesium.ConstantProperty(outlineColor);
+    }
+    
+}
+
+
+
+//---------------------------------------------INFOBOX------------------------------------------------
+function infoBox(loaded){
+	var numPolygons = geojsonDataSource.entities.entities.length;	
+	
+	if(numPolygons > 0 && loaded){	
+		var numVertex = 0;
+		for(var i=0; i<numPolygons; i++){
+			numVertex += geojsonDataSource.entities.entities[i].polygon.positions.getValue().length;
+		}
+			
+		var str = 'Number of Polygons: '+ numPolygons +'</br>'+ 
+				  'Number of Outlines: '+ numPolygons +'</br>'+
+				  'Number of Vertices: '+ numVertex;
+		$( '#infoBox' ).html( str );
+	}
+	else{		
+		var str = 'Zero Opportunities Availables In This Date Range';
+		$( '#infoBox' ).html( str );
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
